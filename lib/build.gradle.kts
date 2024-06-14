24101474479d2cef3b07d523055a3e0fdefc9813
Konstantin.Daugavet <konstantin.daugavet@quartett-mobile.de>
2024-06-14 14:42:31 +0200

    Added required pom properties.

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -17,6 +17,30 @@ publishing {
             version = "1.0"
 
             from(components["java"])
+
+            pom.withXml {
+                val root = asNode()
+                root.appendNode("name", project.name)
+                root.appendNode("description", "XTEA Kotlin implementation")
+                root.appendNode("url", "https://github.com/Quartettmobile/xtea-kt")
+                root.appendNode("licenses").apply {
+                    appendNode("license").apply {
+                        appendNode("name", "MIT License")
+                        appendNode("url", "https://opensource.org/license/mit")
+                    }
+                }
+                root.appendNode("developers").apply {
+                    appendNode("developer").apply {
+                        appendNode("id", "Quartett mobile GmbH")
+                        appendNode("name", "Quartett mobile")
+                        appendNode("organization", "Quartett mobile")
+                        appendNode("organizationUrl", "https://quartett-mobile.de")
+                    }
+                }
+                root.appendNode("scm").apply {
+                    appendNode("url", "https://github.com/Quartettmobile/xtea-kt.git")
+                }
+            }
         }
     }
 }
